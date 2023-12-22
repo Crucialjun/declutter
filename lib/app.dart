2e5c1219e00dc631aa1bdeeddccd67db945b23d9
nich.otieno <nicholas.otieno@revca.tech>
2023-12-22 07:16:52 +0300

    update

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,8 @@
+import 'package:declutter/core/bloc/main_bloc.dart';
 import 'package:declutter/features/home/home_view.dart';
-import 'package:declutter/theme.dart';
+import 'package:declutter/core/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -8,11 +10,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        debugShowCheckedModeBanner: false,
-        theme: AppTheme.themeData(Brightness.light),
-        darkTheme: AppTheme.themeData(Brightness.dark),
-        home: const HomeView());
+    return BlocProvider(
+      create: (context) => MainBloc(),
+      child: MaterialApp(
+            title: 'Flutter Demo',
+            debugShowCheckedModeBanner: false,
+            theme: AppTheme.themeData(Brightness.light),
+            darkTheme: AppTheme.themeData(Brightness.dark),
+            home: const HomeView()),
+    );
   }
 }
