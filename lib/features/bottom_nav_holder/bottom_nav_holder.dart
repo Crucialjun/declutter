5bdd03fe01b885fbe0dccc5a51d4e77936a3f1af
nicholas.otieno <nicholas.otieno@revca.tech>
2024-01-04 21:02:27 +0300

    Refactor bottom navigation bar in BottomNavHolderView

--- lib/features/bottom_nav_holder/bottom_nav_holder.dart
+++ lib/features/bottom_nav_holder/bottom_nav_holder.dart
@@ -3,30 +3,59 @@ import 'package:declutter/features/home/home_view.dart';
 import 'package:declutter/features/profile/presentation/profile_view.dart';
 import 'package:declutter/features/search/search_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
 
-class BottomNavHolderView extends StatelessWidget {
+class BottomNavHolderView extends StatefulWidget {
   const BottomNavHolderView({super.key});
 
   @override
+  State<BottomNavHolderView> createState() => _BottomNavHolderViewState();
+}
+
+class _BottomNavHolderViewState extends State<BottomNavHolderView> {
+  int selectedIndex = 0;
+  List<Widget> pages = const [
+    HomeView(),
+    SearchView(),
+    CartView(),
+    ProfileView()
+  ];
+  @override
   Widget build(BuildContext context) {
-    PageController pageController = PageController(initialPage: 0);
-    List<Widget> pages = const [
-      HomeView(),
-      SearchView(),
-      CartView(),
-      ProfileView()
-    ];
+    PageController? pageController =
+        PageController(initialPage: 0, keepPage: true);
     return Scaffold(
       body: PageView(
+          onPageChanged: (value) {
+            selectedIndex = value;
+          },
           physics: const NeverScrollableScrollPhysics(),
           controller: pageController,
           children: pages),
       bottomNavigationBar: NavigationBar(
-        destinations: pages.map((e) {
-          return const NavigationDestination(
-              icon: Icon(Icons.home), label: "Home");
-        }).toList(),
+        selectedIndex: selectedIndex,
+        destinations: const [
+          NavigationDestination(
+              selectedIcon: Icon(IconlyBold.home),
+              icon: Icon(IconlyLight.home),
+              label: "Home"),
+          NavigationDestination(
+              selectedIcon: Icon(IconlyBold.search),
+              icon: Icon(IconlyLight.search),
+              label: "Search"),
+          NavigationDestination(
+              selectedIcon: Icon(IconlyBold.bag2),
+              icon: Icon(IconlyLight.bag2),
+              label: "Cart"),
+          NavigationDestination(
+              selectedIcon: Icon(IconlyBold.profile),
+              icon: Icon(IconlyLight.profile),
+              label: "Profile"),
+        ],
         onDestinationSelected: (value) {
+          setState(() {
+            selectedIndex = value;
+          });
           pageController.jumpToPage(value);
         },
       ),
