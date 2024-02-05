9420bf5665bcbc714efa96d4592d37237f6bba2d
nich.otieno <nicholas.otieno@revca.tech>
2024-02-05 23:56:47 +0300

    Add FancyShimmerImage to CartView

--- lib/features/cart/presentation/cart_view.dart
+++ lib/features/cart/presentation/cart_view.dart
@@ -1,5 +1,6 @@
 import 'package:declutter/core/app_assets.dart';
 import 'package:declutter/features/cart/presentation/widgets/empty_cart.dart';
+import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
@@ -29,7 +30,24 @@ class CartView extends StatelessWidget {
         itemBuilder: (context, index) {
           return Row(
             children: [
-             
+              FancyShimmerImage(
+                imageUrl:
+                    "https://images.pexels.com/photos/697509/pexels-photo-697509.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                boxFit: BoxFit.cover,
+                width: 100,
+                height: 100,
+              ),
+              SizedBox(width: 10.w),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Product Name',
+                      style: Theme.of(context).textTheme.titleMedium),
+                  Text('Price', style: Theme.of(context).textTheme.titleMedium),
+                  Text('Quantity',
+                      style: Theme.of(context).textTheme.titleMedium),
+                ],
+              ),
             ],
           );
         },
