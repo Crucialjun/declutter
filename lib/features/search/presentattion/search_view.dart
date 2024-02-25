6dc640bd5212fb277ef158d73c86018b93f8b592
nich.otieno <nicholas.otieno@revca.tech>
2024-02-25 17:59:35 +0300

    Add dynamic height grid view and fancy shimmer image

--- lib/features/search/presentattion/search_view.dart
+++ lib/features/search/presentattion/search_view.dart
@@ -1,5 +1,9 @@
 import 'package:declutter/core/app_assets.dart';
+import 'package:dynamic_height_grid_view/dynamic_height_grid_view.dart';
+import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:logger/logger.dart';
 
 class SearchView extends StatefulWidget {
@@ -54,7 +58,68 @@ class _SearchViewState extends State<SearchView> {
                   onFieldSubmitted: (value) {
                     Logger().d(searchController.text);
                   },
-                )
+                ),
+                SizedBox(
+                  height: 16.h,
+                ),
+                Expanded(
+                  child: DynamicHeightGridView(
+                    crossAxisCount: 2,
+                    itemCount: 30,
+                    builder: (context, index) {
+                      return InkWell(
+                        onTap: () {
+                          Logger().d('Product $index');
+                        },
+                        child: Card(
+                          child: Column(
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(8.0),
+                                child: FancyShimmerImage(
+                                  height: 120.h,
+                                  imageUrl: 'https://picsum.photos/200/300',
+                                  boxFit: BoxFit.cover,
+                                  errorWidget: const Icon(Icons.error),
+                                ),
+                              ),
+                              Row(
+                                children: [
+                                  Flexible(
+                                    flex: 5,
+                                    child: Text(
+                                      'Product $index' * 10,
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .titleLarge,
+                                    ),
+                                  ),
+                                  IconButton(
+                                      onPressed: () {},
+                                      icon: const Icon(Icons.favorite_border)),
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text("Price: 1000",
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .titleMedium),
+                                  IconButton(
+                                      onPressed: () {},
+                                      icon: const Icon(
+                                          Icons.add_shopping_cart_outlined))
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
               ],
             ),
           )),
