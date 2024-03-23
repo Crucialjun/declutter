2275ccac6845d348c6affb05d7dbdf3dab782a70
nich.otieno <nicholas.otieno@revca.tech>
2024-03-24 00:14:27 +0300

    Add imports for AppAssets, AppNameText, and FlutterScreenUtil in home_view.dart*** ***Update dependencies in pubspec.yaml

--- lib/features/home/home_view.dart
+++ lib/features/home/home_view.dart
@@ -1,6 +1,9 @@
+import 'package:declutter/core/app_assets.dart';
 import 'package:declutter/core/bloc/main_bloc.dart';
+import 'package:declutter/utils/widgets/app_name_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -8,24 +11,31 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const Text(
-          "Home View",
-          style: TextStyle(fontSize: 50, color: Colors.red),
+        appBar: AppBar(
+          leading: Padding(
+            padding: EdgeInsets.only(left: 8.w),
+            child: Image.asset(AppAssets.shoppingCart),
+          ),
+          title: const AppNameText(),
         ),
-        BlocBuilder<MainBloc, MainState>(
-          builder: (context, state) {
-            return SwitchListTile(
-                title: Text(state.isDarkTheme ? 'Dark' : 'Light'),
-                value: state.isDarkTheme,
-                onChanged: (value) {
-                  context.read<MainBloc>().add(SetDarkTheme(value));
-                });
-          },
-        )
-      ],
-    ));
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Home View",
+              style: TextStyle(fontSize: 50, color: Colors.red),
+            ),
+            BlocBuilder<MainBloc, MainState>(
+              builder: (context, state) {
+                return SwitchListTile(
+                    title: Text(state.isDarkTheme ? 'Dark' : 'Light'),
+                    value: state.isDarkTheme,
+                    onChanged: (value) {
+                      context.read<MainBloc>().add(SetDarkTheme(value));
+                    });
+              },
+            )
+          ],
+        ));
   }
 }
